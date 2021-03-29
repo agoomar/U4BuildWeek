fdbb508096781ff67e7a74ae9bd7ba7792723591
agoomar <agoomar1996@gmail.com>
2021-03-28 21:32:57 -0500

    Implemented tables, need to fix

---
+++ api/data/migrations/20210329020414_create-diners-operators.js
@@ -0,0 +1,33 @@
+exports.up = function (knex) {
+	return knex.schema
+		.createTable('diners', (tbl) => {
+			tbl.increments();
+			tbl
+				.integer('userId')
+				.unique()
+				.unsigned()
+				.notNullable()
+				.references('users_id')
+				.onDelete('CASCADE')
+				.onUpdate('CASCADE');
+			tbl
+				.string('currentLocation', 256)
+				.notNullable()
+				.defaultTo('41.881832, -87.623177');
+		})
+		.createTable('operators', (tbl) => {
+			tbl.increments();
+			tbl
+				.integer('userId')
+				.unique()
+				.unsigned()
+				.notNullable()
+				.references()
+				.onDelete('CASCADE')
+				.onUpdate('CASCADE');
+		});
+};
+
+exports.down = function (knex) {
+	return knex.schema.dropTableIfExists('operators').dropTableIfExists('diners');
+};
