9816bee5cc313c579d90fd417f8790d41c5fc8fd
agoomar <agoomar1996@gmail.com>
2021-03-28 21:44:05 -0500

    changes to server.js

--- api/server.js
+++ api/server.js
@@ -1,10 +1,24 @@
-const express = require('express')
-const helmet = require('helmet')
-const cors = require('cors')
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 
-const server = express()
-server.use(express.json())
-server.use(helmet())
-server.use(cors())
+const server = express();
+const authRouter = require('../auth/auth-router');
+const dinersRouter = require('../diners/diners-router');
+const operatorsRouter = require('../operators/operators-router.js');
+const trucksRouter = require('../trucks/trucks-router');
 
-module.exports = server
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+
+server.use('/api/auth', authRouter);
+server.use('/api/diners', dinersRouter);
+server.use('/api/operators', operatorsRouter);
+server.use('/api/trucks', trucksRouter);
+
+server.get('/', (req, res) => {
+	res.json({ api: 'up' });
+});
+
+module.exports = server;
